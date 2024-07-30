174165c09d89eccd167b4441e0040d39e0542c18
rafay0000 <itsrafay03@gmail.com>
2024-07-30 07:52:50 +0500

    It is our main Project

---
+++ app/src/main/java/pk/org/cas/finalproject/MainActivity.java
@@ -0,0 +1,20 @@
+package pk.org.cas.finalproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+// This is the Home page of our app.
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
