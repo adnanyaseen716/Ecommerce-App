613dc37fd3f041ebce535704811b0380379b4daa
rafay0000 <itsrafay03@gmail.com>
2024-08-02 22:48:49 +0500

    Home Fragment is done.

--- app/src/main/java/pk/org/cas/finalproject/MainActivity.java
+++ app/src/main/java/pk/org/cas/finalproject/MainActivity.java
@@ -1,20 +1,62 @@
 package pk.org.cas.finalproject;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 // This is the Home page of our app.
 public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frameLayout = findViewById(R.id.frameLayout);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
 
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+
+                if(itemId == R.id.home){
+                    loadFragment(new HomeFragment());
+                }
+                if(itemId == R.id.cart){
+                    loadFragment(new CartFragment());
+                }
+                if(itemId == R.id.favourite){
+                    loadFragment(new FavouriteFragment());
+                }
+                if(itemId == R.id.profile){
+                    loadFragment(new ProfileFragment());
+                }
+
+                return true;
+            }
+        });
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
     }
 }
